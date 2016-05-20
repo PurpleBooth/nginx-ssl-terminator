510438045764dee2265a527f20f817922c485840
Billie Thompson <billie@purplebooth.co.uk>
2016-05-20 23:35:51 +0100

    Added ability to set server name for SNI

--- run.sh
+++ run.sh
@@ -10,5 +10,14 @@ if [ -f ${SSL_CRT_PATH:-/..} ] ; then
     cp $SSL_CRT_PATH /certificate.crt
 fi
 
+
+
+if [ -z "$SERVER_NAME" ] ; then
+    sed -i"" "s#XXXXXXX##g" /etc/nginx/nginx.conf
+else
+    sed -i"" "s#XXXXXXX#server_name $SERVER_NAME;#g" /etc/nginx/nginx.conf
+fi
+
 sed -i"" "s#~~~~~~~#$UPSTREAM#g" /etc/nginx/nginx.conf
+
 exec nginx -g "daemon off; error_log /dev/stderr info;"
